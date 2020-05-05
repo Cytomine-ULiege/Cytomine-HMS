2e61aa77f7412a748659d23400dee47a51ce3950
Ulysse Rubens <urubens@uliege.be>
2020-05-05 15:42:43 +0200

    Add profile reading endpoints

---
+++ cytomine-hms/app.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+# * Copyright (c) 2009-2020. Authors: see NOTICE file.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+
+import json
+from io import BytesIO
+
+import h5py
+import numpy as np
+from PIL import Image
+from shapely import wkt
+
+from .reader import prepare_geometry, prepare_slices, get_mask, extract_profile, get_cartesian_indexes, \
+    get_projection, get_bounds
+from .utils import NumpyEncoder
+from flask import Flask, abort, request, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/profile.json')
+def get_profile():
+    path = request.args.get('path')
+    geometry = wkt.loads(request.args.get('geometry'))
+    if path is None or geometry is None:
+        abort(400)
+
+    min_slice = request.args.get('minSlice', None, type=int)
+    max_slice = request.args.get('maxSlice', None, type=int)
+
+    hdf5 = h5py.File(path, 'r')
+    geometry = prepare_geometry(hdf5, geometry)
+    slices = prepare_slices(hdf5, min_slice, max_slice)
+
+    mask = get_mask(hdf5, geometry)
+    profile = extract_profile(hdf5, mask, slices)
+    profile_mask = mask[get_bounds(mask)]
+    profile = profile[profile_mask.nonzero()]
+
+    X, Y = get_cartesian_indexes(hdf5, mask)
+    response = []
+    for x, y, data in zip(X, Y, profile):
+        response.append({
+            "point": [x, y],
+            "profile": data
+        })
+
+    return json.dumps(response, cls=NumpyEncoder, check_circular=False)
+
+
+@app.route('/profile/stats.json')
+def get_profile_stats():
+    path = request.args.get('path')
+    geometry = wkt.loads(request.args.get('geometry'))
+    if path is None or geometry is None:
+        abort(400)
+
+    min_slice = request.args.get('minSlice', None, type=int)
+    max_slice = request.args.get('maxSlice', None, type=int)
+
+    hdf5 = h5py.File(path, 'r')
+    geometry = prepare_geometry(hdf5, geometry)
+    slices = prepare_slices(hdf5, min_slice, max_slice)
+
+    mask = get_mask(hdf5, geometry)
+    profile = extract_profile(hdf5, mask, slices)
+    profile_mask = mask[get_bounds(mask)]
+    profile = profile[profile_mask.nonzero()]
+
+    minimums = get_projection(profile, np.min)
+    maximums = get_projection(profile, np.max)
+    averages = get_projection(profile, np.mean)
+
+    X, Y = get_cartesian_indexes(hdf5, mask)
+    response = []
+    for x, y, mini, maxi, avg in zip(X, Y, minimums, maximums, averages):
+        response.append({
+            "point": [x, y],
+            "min": mini,
+            "max": maxi,
+            "average": avg
+        })
+
+    return json.dumps(response, cls=NumpyEncoder, check_circular=False)
+
+
+@app.route('/profile/min-projection.<format>')
+def get_profile_min_projection(format):
+    return _get_profile_image_projection(np.min, format)
+
+
+@app.route('/profile/max-projection.<format>')
+def get_profile_max_projection(format):
+    return _get_profile_image_projection(np.max, format)
+
+
+@app.route('/profile/average-projection.<format>')
+def get_profile_average_projection(format):
+    return _get_profile_image_projection(np.mean, format)
+
+
+def _get_profile_image_projection(proj_func, format):
+    path = request.args.get('path')
+    geometry = wkt.loads(request.args.get('geometry'))
+    if path is None or geometry is None:
+        abort(400)
+
+    min_slice = request.args.get('minSlice', None, type=int)
+    max_slice = request.args.get('maxSlice', None, type=int)
+
+    hdf5 = h5py.File(path, 'r')
+    geometry = prepare_geometry(hdf5, geometry)
+    slices = prepare_slices(hdf5, min_slice, max_slice)
+
+    mask = get_mask(hdf5, geometry)
+    profile = extract_profile(hdf5, mask, slices)
+
+    projection = get_projection(profile, proj_func)
+    masked_projection = projection * mask[get_bounds(mask)]
+
+    bpc = hdf5['bpc'][()]
+    if bpc > 8 or format not in ['jpg', 'png']:
+        format = 'png'
+
+    img = Image.fromarray(masked_projection)
+    img_io = BytesIO()
+    img.save(img_io, format)
+    img_io.seek(0)
+    mime_type = "image/jpeg" if format == "jpg" else "image/png"
+    return send_file(img_io, mimetype=mime_type)
+
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ cytomine-hms/reader.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+# * Copyright (c) 2009-2020. Authors: see NOTICE file.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+
+import numpy as np
+from rasterio.features import geometry_mask
+from rasterio.transform import IDENTITY
+from shapely.affinity import affine_transform
+from shapely.geometry import box, Point, LineString
+
+
+def change_referential(geometry, height):
+    """
+    Return the geometry given in cartesian coordinate system to a matrix-like coordinate system.
+    :param height: The height of the image having this geometry
+    :param geometry: The geometry in cartesian coordinate system
+    :return: The geometry in matrix-like coordinate system
+    """
+
+    if type(geometry) in [Point, LineString]:
+        matrix = [1, 0, 0, -1, 0, height - 1]
+    else:
+        matrix = [1, 0, 0, -1, 0, height]
+
+    return affine_transform(geometry, matrix)
+
+
+def prepare_geometry(hdf5, geometry):
+    """
+    Get a valid geometry in matrix-like coordinate system
+    :param hdf5: The HDF5 file the geometry must be valid for
+    :param geometry: The geometry in cartesian coordinate system
+    :return: A valid geometry in matrix-like coordinate system
+    """
+    image_width = hdf5['width'][()]
+    image_height = hdf5['height'][()]
+    image_geometry = box(0, 0, image_width, image_height)
+    return change_referential(geometry.intersection(image_geometry), image_height)
+
+
+def prepare_slices(hdf5, min_slice, max_slice):
+    n_slices = hdf5['nSlices'][()]
+
+    if not min_slice or min_slice < 0 or min_slice > n_slices:
+        min_slice = 0
+
+    if not max_slice or max_slice < min_slice or max_slice > n_slices:
+        max_slice = n_slices
+
+    return min_slice, max_slice
+
+
+def get_mask(hdf5, geometry):
+    image_width = hdf5['width'][()]
+    image_height = hdf5['height'][()]
+    return geometry_mask([geometry], (image_height, image_width), transform=IDENTITY, invert=True)
+
+
+def get_bounds(mask):
+    i, j = np.nonzero(mask)
+    return np.s_[np.min(i):np.max(i)+1, np.min(j):np.max(j)+1]
+
+
+def extract_profile(hdf5, mask, slices):
+    """
+    Get profile data as matrix
+    :param hdf5: The HD5 file with profile data
+    :param mask: The geometry mask
+    :param slices: A Python slice of image slices
+    :return:
+    """
+    bounds = get_bounds(mask) + (slice(*slices),)
+    return hdf5['data'][bounds]
+
+
+def get_projection(profile, proj_func):
+    return proj_func(profile, axis=-1)
+
+
+def get_cartesian_indexes(hdf5, mask):
+    image_height = hdf5['height'][()]
+    y_indexes, x_indexes = mask.nonzero()
+    y_indexes = image_height - 1 - y_indexes
+
+    return x_indexes, y_indexes
