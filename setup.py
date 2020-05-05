a536e6b25d7271adf594ff762c027f8436870ce3
Ulysse Rubens <urubens@uliege.be>
2020-05-05 15:42:11 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cytomine-hms',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'requests',
+        'rasterio',
+        'h5py',
+        'shapely',
+        'numpy',
+        'pillow'
+    ],
+)
